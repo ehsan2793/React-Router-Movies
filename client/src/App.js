5446ea554be3cdd24353ba38dd69b49f6ad0ff4f
ehsan2793 <ehsan2793@gmail.com>
2021-08-16 21:57:01 -0700

    name show up but some part still missing need more time to work on

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route ,Switch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
@@ -11,7 +12,10 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(response => { 
+          console.log(response);
+          setMovieList(response.data) 
+
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -29,8 +33,17 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Switch>
+        <Route exact path="/" >
+          <MovieList movies = {movieList}/>
+        </Route>
 
-      <div>Replace this Div with your Routes</div>
+        <Route path="/movies/:id" >
+            <Movie/>
+       </Route>
+          
+      </Switch> 
+          
     </div>
   );
 }
